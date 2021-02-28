76ea300e28ed5748df8e0aec490def5583dd8128
owenmoogk <owenmoogk@gmail.com>
2021-02-28 00:14:30 -0500

    added datalogging and graphing
    this took WAY TOO LONG. like all of today. and yesterday night. but it works. sort of. get off my back :/

---
+++ data.py
@@ -0,0 +1,8 @@
+data = {
+    "x": [],
+    "y": [],
+    "angle": [],
+    "velocity": [],
+    "acceleration": [],
+    "ticks": [],
+}
--- main.py
+++ main.py
@@ -1,22 +1,52 @@
-# INIT
-import pygame, time
+import pygame, time, threading
 from settings import *
 from pendulum import *
 from math import sin
 from drawWindow import *
-
+import data
+import matplotlib.pyplot as plt
 # globals
 clock = pygame.time.Clock()
 pendulum = Pendulum()
 
-while True:
-    clock.tick(simSpeed)
 
-    # quit function
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+plt.ion()
 
-    pendulum.update()
-    drawWindow(screen, pendulum)
+x = data.data["x"]
+y = data.data["y"]
+
+figure, ax = plt.subplots()
+line1, = ax.plot(x, y)
+
+def updateGraph():
+    x = data.data["ticks"]
+    y = data.data["angle"]
+
+    line1.set_xdata(x)
+    line1.set_ydata(y)
+    ax.margins(1000)
+
+    figure.canvas.draw()
+    figure.canvas.flush_events()
+
+def main():
+    tick = 0
+    while True:
+        tick += 1
+        clock.tick(simSpeed)
+
+        # quit function
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                data.running = False
+                pygame.quit()
+
+        pendulum.update()
+        updateGraph()
+        drawWindow(screen, pendulum)
+        data.data["x"].append(pendulum.nodeX - pendulum.originX)
+        data.data["y"].append(-(pendulum.nodeY - pendulum.originY))
+        data.data["ticks"].append(tick)
+        data.data["angle"].append(pendulum.angle)
+
+main()
